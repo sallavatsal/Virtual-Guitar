73965309acbde36c57dcc05f0f74edb9549367f7
Vatsal Salla <sallavatsal@gmail.com>
2018-05-23 20:38:39 -0500

    Update code.py

--- code.py
+++ code.py
@@ -34,19 +34,58 @@ while 1:
 	a0=0
 
 	if GPIO.input(7): a1=1
-	else:	a1=0	if GPIO.input(11): a2=1	else:	a2=0	if GPIO.input(12): a3=1	else:	a3=0	if GPIO.input(13): a4=1	else:	a4=0	if GPIO.input(15): a5=1	else:	a5=0	if GPIO.input(16): a6=1	else:	a6=0	if GPIO.input(18): a7=1	else:	a7=0
-x = a1*1 + a2*4 + a2*8 + a3*16 + a4*32 + a5*64 + a6*128 + a7*255if x<42:	pygame.mixer.music.load("2.wav")	pygame.mixer.music.play()	while pygame.mixer.music.get_busy() == True:
+	else:	a1=0
 		
-	continueelif x>42 and x<84:
+	if GPIO.input(11): a2=1
+	else:	a2=0
+		
+	if GPIO.input(12): a3=1
+	else:	a3=0
+	
+	if GPIO.input(13): a4=1
+	else:	a4=0
+	
+	if GPIO.input(15): a5=1
+	else:	a5=0
+	
+	if GPIO.input(16): a6=1
+	else:	a6=0
+		
+	if GPIO.input(18): a7=1
+	else:	a7=0
+		
+	x = a1*1 + a2*4 + a2*8 + a3*16 + a4*32 + a5*64 + a6*128 + a7*255if x<42:
+	pygame.mixer.music.load("2.wav")
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:	
+	continue
+	
+	elif x>42 and x<84:
 	pygame.mixer.music.load("1.wav")
-	pygame.mixer.music.play()	while pygame.mixer.music.get_busy() == True:
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:
+	continue
 
-	continueelif x>84 and x<126:
+	elif x>84 and x<126:
 	pygame.mixer.music.load("3rd_String_G_64kb.wav")
-	pygame.mixer.music.play()	while pygame.mixer.music.get_busy() == True:
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:
+	continue
 
-	continueelif x>126 and x<168: pygame.mixer.music.load("4th_String_D_64kb.wav")
-	pygame.mixer.music.play()	while pygame.mixer.music.get_busy() == True:	
-	continueelif x>168 and x<210:	pygame.mixer.music.load("5th_string_A_64kb.wav")	pygame.mixer.music.play()	while pygame.mixer.music.get_busy() == True:	
-	continueelif x>210:	pygame.mixer.music.load("6th_String_E_64kb.wav")	pygame.mixer.music.play()	while pygame.mixer.music.get_busy() == True:	
+	elif x>126 and x<168:
+	pygame.mixer.music.load("4th_String_D_64kb.wav")
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:	
+	continue
+	
+	elif x>168 and x<210:
+	pygame.mixer.music.load("5th_string_A_64kb.wav")
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:	
+	continue
+
+	elif x>210:
+	pygame.mixer.music.load("6th_String_E_64kb.wav")
+	pygame.mixer.music.play()
+	while pygame.mixer.music.get_busy() == True:	
 	continue
